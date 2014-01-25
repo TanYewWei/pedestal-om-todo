fe8c65c020d17ae39a140957e8c796c00fcbb0d7
Yew-wei Tan <yewwei.tan@gmail.com>
2014-01-25 11:54:47 +1100

    commit first

--- project.clj
+++ project.clj
@@ -1,4 +1,11 @@
 (defproject pedestal-om-todo "0.0.1-SNAPSHOT"
+  :cljsbuild {:builds [{:source-paths ["app" "test"]
+                        :compiler {:output-to "target/cljs/testable.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]
+              :test-commands {"unit-tests" ["phantomjs" :runner
+                                            "window.literal_js_was_evaluated=true"
+                                            "target/cljs/testable.js"]}}
   :description "FIXME: write description"
   :dependencies [[com.cemerick/piggieback "0.1.0"]
                  [ch.qos.logback/logback-classic "1.0.13" :exclusions [org.slf4j/slf4j-api]]                 
@@ -12,11 +19,10 @@
                  [org.clojure/clojurescript "0.0-2138"]
                  [prismatic/schema "0.2.0"]
                  [sablono "0.2.1"]
-                 [secretary "0.4.0"]]
+                 [secretary "0.4.0"]]    
   :min-lein-version "2.0.0"
-  :source-paths ["app/src" "app/templates"]
-  :resource-paths ["config"]
-  :target-path "out/"
+  :plugins [[lein-cljsbuild "1.0.0"]
+            [com.cemerick/clojurescript.test "0.2.1"]]
   :repl-options  {:init-ns user
                   :init (try
                           (use 'io.pedestal.app-tools.dev)
@@ -26,4 +32,8 @@
                             (println)))
                   :welcome (println "Welcome to pedestal-app! Run (tools-help) to see a list of useful functions.")
                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  :resource-paths ["config"]
+  :source-paths ["app/src" "app/templates"]
+  :target-path "out/"
+  
   :main ^{:skip-aot true} io.pedestal.app-tools.dev)
